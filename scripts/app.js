078ed8ea7c0b42537e1a86d153affedf73d42a3a
Ayo Afolabi <ayo.afolabi.24@gmail.com>
2022-05-10 23:39:25 +0100

    functionality completed with styling

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,8 @@
 const timeRemaining = document.querySelector('#time-left')
 const score = document.querySelector('#score')
 // const lives = document.querySelector('#lives')
+
+const playingAudio = document.querySelector('#playingAudio')
 const start = document.querySelector('#start-button')
 
 const grid = document.querySelectorAll('.grid div')
@@ -13,12 +15,10 @@ const blackWidowRight = document.querySelectorAll('.blackWidow-right')
 const ironmanLeft = document.querySelectorAll('.ironman-left')
 const hulkRight = document.querySelectorAll('.hulk-right')
 
-
-//const thanosStartPosition = 115
 let thanosCurrentPosition = 115
 const width = 11
 let timer
-let currentTime = 20
+let currentTime = 30
 
 function moveThanos(e) {
   //console.log(e)
@@ -80,29 +80,7 @@ function moveIronmanLeft(ironmanLeft) {
   } else {
     console.log('NO IRONMAN MOVEMENT')
   }
-
-  // Alternative to use, but I manageged to get the if statement working.
-
-  // switch (true) {
-  //   case ironmanLeft.classList.contains('l1'):
-  //     ironmanLeft.classList.remove('l1')
-  //     ironmanLeft.classList.add('l2')
-  //     break
-  //   case ironmanLeft.classList.contains('l2'):
-  //     ironmanLeft.classList.remove('l2')
-  //     ironmanLeft.classList.add('l3')
-  //     break
-  //   case ironmanLeft.classList.contains('l3'):
-  //     ironmanLeft.classList.remove('l3')
-  //     ironmanLeft.classList.add('l4')
-  //     break
-  //   case ironmanLeft.classList.contains('l4'):
-  //     ironmanLeft.classList.remove('l4')
-  //     ironmanLeft.classList.add('l1')
-  //     break
-  // }
 }
-
 
 function autoMove2() {
   hulkRight.forEach(hulkRight => moveHulkRight(hulkRight))
@@ -274,76 +252,23 @@ function stoneReached() {
   }
 }
 
+function playsAudio() {
+  playingAudio.play()
+  playingAudio.loop = true
+}
+
 start.addEventListener('click', () => {
   if (timer) {
     clearInterval(timer)
     timer = null
     document.removeEventListener('keydown', moveThanos)
   } else {
+    start.disabled = true
     timer = setInterval(autoMove1, 1000)
     setInterval(autoMove2, 750)
     setInterval(autoMove3, 1250)
     document.addEventListener('keydown', moveThanos)
+    playsAudio()
   }
 })
 
-
-// function init() {
-
-//   const grid = document.querySelector('.grid')
-
-//   const width = 10
-//   const cellCount = width * width
-//   const cells = []
-
-//   const thanosClass = 'thanos'
-//   const thanosStartPosition = 5
-//   let thanosCurrentPosition = 5
-
-//   function createGrid() {
-//     for (let i = 0; i < cellCount; i++) {
-//       const cell = document.createElement('div')
-//       cell.innerText = i
-//       grid.appendChild(cell)
-//       cells.push(cell)
-//     }
-//     addThanos(thanosStartPosition)
-//   }
-
-//   function addThanos(position) {
-//     console.log('Position being passed in', position)
-//     console.log('cell picked based on the position index being passed in', cells[position])
-//     cells[position].classList.add(thanosClass)
-//   }
-//   function removeThanos(position) {
-//     cells[position].classList.remove(thanosClass)
-//   }
-
-//   function handleKeyDown(event) {
-//     const key = event.keyCode
-//     const left = 37
-//     const right = 39
-//     const up = 38
-//     const down = 40
-//     console.log('position before redefining', thanosCurrentPosition)
-//     removeThanos(thanosCurrentPosition)
-
-//     if (key === right && thanosCurrentPosition % width !== width - 1) {
-//       thanosCurrentPosition++
-//     } else if (key === left && thanosCurrentPosition % width !== 0) {
-//       thanosCurrentPosition--
-//     } else if (key === up && thanosCurrentPosition >= width) {
-//       thanosCurrentPosition -= width
-//     } else if (key === down && thanosCurrentPosition + width <= cellCount - 1) {
-//       thanosCurrentPosition += width
-//     } else {
-//       console.log('INVALID KEY')
-//     }
-//     console.log('position after redefining', thanosCurrentPosition)
-//     addThanos(thanosCurrentPosition)
-//   }
-//   document.addEventListener('keydown', handleKeyDown)
-
-//   createGrid(thanosStartPosition)
-// }
-// window.addEventListener('DOMContentLoaded', init)
